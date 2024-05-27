2bbc3e1cf98b97971108ac2bedd3b21160f0bcd4
matsumori-m <matsumori.m@selecty-hd.co.jp>
2024-05-27 16:48:06 +0900

    登録確認　作業中

---
+++ src/main/java/action/Insert.java
@@ -0,0 +1,24 @@
+package action;
+
+import javax.servlet.annotation.WebServlet;
+
+/**
+ * Servlet implementation class Insert
+ */
+@WebServlet("/Insert")
+public class Insert extends BaseServlet {
+	protected String doAction()throws Exception {
+
+		String[] pageParam = super.getInputParameter(
+				 "userNm"		// 0
+				,"pass"			// 1
+				,"passConfirm"	// 2
+		);
+
+		if (!pageParam[1].equals(pageParam[2])) {
+			throw new Exception("パスワードが一致していません");
+		}
+
+		return "insertResult";
+	}
+}
