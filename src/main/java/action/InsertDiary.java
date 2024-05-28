528f940b22aea09b007bb434752862c1282227f5
matsumori-m <matsumori.m@selecty-hd.co.jp>
2024-05-28 13:54:52 +0900

    日記記入

---
+++ src/main/java/action/InsertDiary.java
@@ -0,0 +1,13 @@
+package action;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Servlet implementation class InsertDiary
+ */
+@WebServlet("/InsertDiary")
+public class InsertDiary extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+}
