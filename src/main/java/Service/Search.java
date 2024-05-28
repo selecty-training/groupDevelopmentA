746e671e591da44fce52f10acb25f0914d6eb715
matsumori-m <matsumori.m@selecty-hd.co.jp>
2024-05-28 16:15:03 +0900

    DB接続・切断変更

--- src/main/java/Service/Search.java
+++ src/main/java/Service/Search.java
@@ -7,6 +7,9 @@ import java.util.Map;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 
+import dao.UserDao;
+import util.DBUtil;
+
 
 @WebServlet("/Search")
 public class Search extends HttpServlet {
@@ -16,12 +19,12 @@ public class Search extends HttpServlet {
 	public Map<String, Object> doLogin(String userNm, String pass) throws Exception {
 		Map<String, Object> loginData = new HashMap<>();
 	
-//		EmployeeDao dao = new EmployeeDao(this.con);
-//		try {
-//			loginData.put("account", dao.selectLoginInfo(userNm, pass));
-//		} finally {
-//			DbUtil.closeConnection(this.con);
-//		}
+		UserDao dao = new UserDao(this.con);
+		try {
+			loginData.put("account", dao.selectLoginInfo(userNm, pass));
+		} finally {
+			DBUtil.closeConnection(this.con);
+		}
 		return loginData;
 	}
 }
