771fcb4f653227d31deaea5133948976b6e8b77b
matsumori-m <matsumori.m@selecty-hd.co.jp>
2024-05-28 13:54:02 +0900

    パスワード確認

---
+++ src/main/java/action/login.java
@@ -0,0 +1,27 @@
+package action;
+
+import java.util.Map;
+
+import javax.servlet.annotation.WebServlet;
+
+import Service.Search;
+
+@WebServlet("/login")
+public class login extends BaseServlet {
+	protected String doAction() throws Exception {
+		
+		String[] param = super.getInputParameter("userNm", "pass");
+
+		Search service = new Search();
+		Map<String, Object> loginData = service.doLogin(param[0], param[1]);
+
+		if (loginData == null || loginData.get("account") == null) {
+			throw new Exception("名前かパスワードが違うよ");
+		}
+
+		super.session.setAttribute("LOGIN", loginData.get("userNm"));
+		return "menu";
+	}
+    
+	
+}
