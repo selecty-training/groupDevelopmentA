15428b69cb4311b567b4de934ec6e789a5caa8ef
Panwar-A <panwaru.a@selecty-hd.co.jp>
2024-05-29 15:07:50 +0900

    更新＆削除ボタンとJS完了

---
+++ src/main/webapp/js/mydata.js
@@ -0,0 +1,16 @@
+// btn selecterを取得
+let btn = document.querySelectorAll(".btn");
+let diaryId = document.querySelector("#diaryId");
+let action =document.querySelector("#action");
+for(let i=0;i<btn.length;i++){
+    btn[i].addEventListener("click",function(){
+       diaryId.value =this.parentNode.children[0].value;
+
+       if(this.innerHTML==="更新"){
+        action.value = "update";
+       }else{
+        action.value = "delete";
+       }
+       document.actionform.submit();
+    });
+}   
