7a25dec3cbca36e2ba5376602d302afb7a644085
ozawa-a <ozawa.a@selecty-hd.co.jp>
2024-05-29 13:36:52 +0900

    Deleteを修正

--- src/main/java/action/Delete.java
+++ src/main/java/action/Delete.java
@@ -1,54 +1,32 @@
 package action;
 
-import java.io.IOException;
-
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+
+import Service.Update;
 
 /**
  * Servlet implementation class Delete
  */
-@WebServlet("/Delete")
+@WebServlet(name="/Delete", urlPatterns={"/delete"})
 public class Delete extends BaseServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Delete() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
 
 	@Override
 	protected String getPageName() {
-		// TODO 自動生成されたメソッド・スタブ
-		return null;
+		return "deleteConfirm";
 	}
 
 	@Override
 	protected String doAction() throws Exception {
-		// TODO 自動生成されたメソッド・スタブ
-		return null;
+		
+		String[] pageParam = super.getInputParameter(
+				 "id_user"		// 0
+		);
+		
+		Update service = new Update();
+		service.deleteUser(Integer.parseInt(pageParam[0]));
+		
+		return "deleteResult";
 	}
 
 }
