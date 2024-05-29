5698add1e77f8b45492fcc9b15848387d157d325
matsumori-m <matsumori.m@selecty-hd.co.jp>
2024-05-29 15:10:03 +0900

    Insertの名前をSiginupに変更、処理の追加、文章の修正

--- src/main/java/action/Signup.java
+++ src/main/java/action/Signup.java
@@ -2,13 +2,20 @@ package action;
 
 import javax.servlet.annotation.WebServlet;
 
+import entity.User;
+
 /**
  * Servlet implementation class Insert
  */
-@WebServlet("/Insert")
-public class Insert extends BaseServlet {
+@WebServlet("/Siginup")
+public class Signup extends BaseServlet {
+	@Override
+	protected String getPageName() {
+		return "insertConfirm";
+	}
+	
 	protected String doAction()throws Exception {
-
+		
 		String[] pageParam = super.getInputParameter(
 				 "userNm"		// 0
 				,"pass"			// 1
@@ -16,14 +23,13 @@ public class Insert extends BaseServlet {
 		);
 
 		if (!pageParam[1].equals(pageParam[2])) {
-			throw new Exception("パスワードが違う");
+			throw new Exception("パスワードが違うよ");
 		}
-
+		
+		User user = new User();
+		user.setNmUser(pageParam[0]);
+		user.setPassword(pageParam[1]);
 		return "insertMenu";
 	}
 
-	@Override
-	protected String getPageName() {
-		return "insertConfirm";
-	}
 }
