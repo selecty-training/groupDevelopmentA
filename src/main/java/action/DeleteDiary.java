3b8b926a3eb4f580bcc6a492e1d1de3d46e01f3c
ozawa-a <ozawa.a@selecty-hd.co.jp>
2024-05-29 13:35:30 +0900

    DeleteDiaryを作成

---
+++ src/main/java/action/DeleteDiary.java
@@ -0,0 +1,32 @@
+package action;
+
+import javax.servlet.annotation.WebServlet;
+
+import Service.Update;
+
+/**
+ * Servlet implementation class DeleteDiary
+ */
+@WebServlet(name="/DeleteDiary", urlPatterns={"/deletediary"})
+public class DeleteDiary extends BaseServlet {
+	private static final long serialVersionUID = 1L;
+       
+    @Override
+	protected String getPageName() {
+		return "deleteConfirmDiary";
+	}
+
+	@Override
+	protected String doAction() throws Exception {
+		
+		String[] pageParam = super.getInputParameter(
+				 "id_diary"		// 0
+		);
+
+		Update service = new Update();
+		service.deleteDiary(Integer.parseInt(pageParam[0]));
+		
+		return "deleteResultDiary";
+	}
+
+}
