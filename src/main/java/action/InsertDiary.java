4b82a7537122884040091940f9d9434cb962f89d
ozawa-a <ozawa.a@selecty-hd.co.jp>
2024-05-31 09:22:16 +0900

    InsertDiaryを作成

--- src/main/java/action/InsertDiary.java
+++ src/main/java/action/InsertDiary.java
@@ -1,13 +1,39 @@
 package action;
 
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
+
+import Service.Update;
+import Service.Update.UPDATE_MODE;
+import entity.Diary;
 
 /**
  * Servlet implementation class InsertDiary
  */
-@WebServlet("/InsertDiary")
-public class InsertDiary extends HttpServlet {
+@WebServlet("/insertDiary")
+public class InsertDiary extends BaseServlet {
 	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected String getPageName() {
+		return "insertDiary";
+	}
+
+	@Override
+	protected String doAction() throws Exception {
+		
+		String[] pageParam = super.getInputParameter(
+				"title"		 	//0
+				,"text"			//1
+		);
+		
+		Diary diary = new Diary();
+		diary.setTitle(pageParam[0]);
+		diary.setText(pageParam[1]);
+		
+		Update service = new Update();
+		service.registDiary(diary, UPDATE_MODE.INSERT);
+		
+		return "menu";
+	}
 
 }
